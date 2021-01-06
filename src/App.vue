891a9344b3ceac00637d6d5ff94fedb7483afce7
cjj <83957315@qq.com>
2021-01-06 11:02:12 +0800

    a

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div id="app">
-    <Player />
+    demo
   </div>
 </template>
 
 <script>
-import Player from 'zw-player'
 export default {
   name: 'app',
   components: {
-    Player
   }
 }
 </script>
